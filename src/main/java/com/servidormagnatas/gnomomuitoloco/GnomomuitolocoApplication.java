690119c91d7fc4c0f7286d6f048f5fb4e2e3a179
GnomoMuitoLoco <gnomomuitoloco@hotmail.com>
2025-12-21 20:41:25 -0300

    Primeiro commit

---
+++ src/main/java/com/servidormagnatas/gnomomuitoloco/GnomomuitolocoApplication.java
@@ -0,0 +1,13 @@
+package com.servidormagnatas.gnomomuitoloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnomomuitolocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnomomuitolocoApplication.class, args);
+	}
+
+}
