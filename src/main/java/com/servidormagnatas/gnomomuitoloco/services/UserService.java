9c6de376e392d23e72033a96b6ed7a5ce3d92dc5
GnomoMuitoLoco <gnomomuitoloco@hotmail.com>
2025-12-21 23:18:22 -0300

    Service layer, component registration

---
+++ src/main/java/com/servidormagnatas/gnomomuitoloco/services/UserService.java
@@ -0,0 +1,25 @@
+package com.servidormagnatas.gnomomuitoloco.services;
+
+import com.servidormagnatas.gnomomuitoloco.entities.User;
+import com.servidormagnatas.gnomomuitoloco.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository respository;
+
+    public List<User> findAll() {
+        return respository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = respository.findById(id);
+        return obj.get();
+    }
+}
