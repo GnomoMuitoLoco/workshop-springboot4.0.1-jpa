b1323e94fa3c5db339841386ef70fa7036176fe9
GnomoMuitoLoco <gnomomuitoloco@hotmail.com>
2025-12-21 21:35:19 -0300

    H2 database, test profile, JPA

--- src/main/java/com/servidormagnatas/gnomomuitoloco/entities/User.java
+++ src/main/java/com/servidormagnatas/gnomomuitoloco/entities/User.java
@@ -1,11 +1,21 @@
 package com.servidormagnatas.gnomomuitoloco.entities;
 
+import jakarta.persistence.*;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import jakarta.persistence.Entity;
 
+@Entity
+@Table (name = "tb_user")
+public class User implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
